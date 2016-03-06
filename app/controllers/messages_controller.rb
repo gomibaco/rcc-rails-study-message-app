a8ce1071b7b0a19cabd10eace129b6f77435f565
Pちゃん <p1ch.jp@gmail.com>
2016-03-06 07:04:18 +0000

    change gemfile for heroku

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,8 +6,17 @@ class MessagesController < ApplicationController
   
   def create
     @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
+    if @message.save
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    else
+      redirect_to root_path, notice: 'メッセージの保存に失敗しました'
+    end
+  end
+  
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
   end
 end
 
